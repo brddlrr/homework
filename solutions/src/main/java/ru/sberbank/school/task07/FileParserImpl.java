ae31d2454d88292231453f24dc10775767ffa3c0
Mikhail Mavrin <mishamavrin@gmail.com>
2019-06-18 05:03:06 +0300

    file parser added

---
+++ solutions/src/main/java/ru/sberbank/school/task07/FileParserImpl.java
@@ -0,0 +1,33 @@
+package ru.sberbank.school.task07;
+
+import lombok.NonNull;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileParserImpl implements FileParser {
+
+
+
+    @Override
+    public List<String> parse(@NonNull String pathToFile) throws FileNotFoundException {
+
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))){
+
+            while (br.ready()) {
+                lines.add(br.readLine());
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return lines;
+    }
+}
